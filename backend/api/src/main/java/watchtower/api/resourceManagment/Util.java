f7db0602c1f50b7aa4dc4daa298976511289e911
nknish <noahknish@gmail.com>
2025-09-20 18:00:13 -0400

    Update distance calculation

--- backend/api/src/main/java/watchtower/api/resourceManagment/Util.java
+++ backend/api/src/main/java/watchtower/api/resourceManagment/Util.java
@@ -3,11 +3,22 @@ package watchtower.api.resourceManagment;
 public class Util {
     public static final double distPerTick = 0.001;
 
-    public static double calculateDistance(double[] pos1, double[] pos2) {
-        if(pos1 == null || pos2 == null) return 0.0;
-
-        double dx = pos2[0] - pos1[0];
-        double dy = pos2[1] - pos1[1];
+    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        double dx = lat2 - lat1;
+        double dy = lon2 - lon1;
         return Math.hypot(dx, dy);
     }
+
+    // public static double[] calculateDirection(double lat1, double lon1, double
+    // lat2, double lon2) {
+    // double dx = lon2 - lon1;
+    // double dy = lat2 - lat1;
+    // double length = Math.sqrt(dx * dx + dy * dy);
+
+    // if (length == 0) {
+    // return new double[]{0.0, 0.0}; // no movement
+    // }
+
+    // return new double[]{dx / length, dy / length};
+    // }
 }
