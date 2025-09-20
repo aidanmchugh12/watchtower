e146ee9aeb5a85bac694e5fea43d7d846ef5afb5
nknish <noahknish@gmail.com>
2025-09-20 17:48:25 -0400

    Update util

--- backend/api/src/main/java/watchtower/api/resourceManagment/Util.java
+++ backend/api/src/main/java/watchtower/api/resourceManagment/Util.java
@@ -1,9 +1,9 @@
 package watchtower.api.resourceManagment;
 
 public class Util {
-    
+    public static final double distPerTick = 0.001;
 
-    public double calculateDistance(double[] pos1, double[] pos2) {
+    public static double calculateDistance(double[] pos1, double[] pos2) {
         if(pos1 == null || pos2 == null) return 0.0;
 
         double dx = pos2[0] - pos1[0];
