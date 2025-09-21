95dfd3b2e90dd756316094972ac2614146a1b312
nknish <noahknish@gmail.com>
2025-09-20 22:09:16 -0400

    Add api endpoints for allocations

--- backend/api/src/main/java/watchtower/api/Controller.java
+++ backend/api/src/main/java/watchtower/api/Controller.java
@@ -8,12 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import watchtower.api.resourceManagment.Disaster;
 import watchtower.api.resourceManagment.Scene;
 import watchtower.api.resourceManagment.Station;
+import watchtower.api.resourceManagment.Unit;
 
 @RestController
 public class Controller {
     Scene s;
+
+    private static class AlgorithmicAllocation {
+        List<Unit> units;
+        Disaster dest;
+    }
+
+    private static class StationAllocation {
+        String stationId;
+        int num;
+        Disaster dest;
+    }
+
+    private static class UnitAllocation {
+        String unitId;
+        Disaster dest;
+    }
 
     @GetMapping("/api/hello")
     public String hello() {
@@ -37,6 +55,28 @@ public class Controller {
         // return
         return "Successfully initialized scene with " + Integer.toString(stations.size()) + " stations with "
                 + Integer.toString(totalUnits) + " total units of capacity.";
+    }    
+
+    @PostMapping("/api/algorithmAllocation")
+    public String algorithmAllocation(@RequestBody AlgorithmicAllocation entity) {
+        s.allocateFromAlgorithm(entity.units, entity.dest);
+        return "Successfully allocated " + Integer.toString(entity.units.size()) + " units to disaster "
+                + entity.dest.getId();
+    }
+
+    @PostMapping("/api/stationAllocation")
+    public String stationAllocation(@RequestBody StationAllocation entity) {
+        s.allocateFromStation(entity.stationId, entity.num, entity.dest);
+        return "Successfully allocated " + Integer.toString(entity.num) + " units from station " + entity.stationId
+                + " to disaster "
+                + entity.dest.getId();
+    }
+
+    @PostMapping("/api/unitAllocation")
+    public String unitAllocation(@RequestBody UnitAllocation entity) {
+        s.allocateFromMoving(entity.unitId, entity.dest);
+        return "Successfully allocated unit " + entity.unitId + " to disaster "
+                + entity.dest.getId();
     }
 
 }
