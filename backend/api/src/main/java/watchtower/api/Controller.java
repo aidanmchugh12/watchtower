fe9af80d736abaecf43a1bd1bb954cb2abd94b3e
nknish <noahknish@gmail.com>
2025-09-20 21:26:34 -0400

    Add non-functional initialize POST

--- backend/api/src/main/java/watchtower/api/Controller.java
+++ backend/api/src/main/java/watchtower/api/Controller.java
@@ -1,13 +1,32 @@
 package watchtower.api;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import watchtower.api.resourceManagment.Scene;
+import watchtower.api.resourceManagment.Station;
 
 @RestController
 public class Controller {
+    Scene s;
 
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from Watchtower!";
     }
+
+    @PostMapping("/api/initializeScene")
+    public String initializeScene(@RequestBody Station[] entity) {
+        List<Station> stations = Arrays.asList(entity);
+
+        s = new Scene(stations);
+
+        return "yippee\n" + entity[0].id;
+    }
+
 }
